5c72fb7c4ad033f0fec9958c3a7158689f088e5a
Olivier Revollat <revollat@gmail.com>
2015-04-05 00:19:46 +0200

    ajout de sonata admin

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,14 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            //new Sonata\IntlBundle\SonataIntlBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
